53419597d9f47ed2bc677c60923ced9fd55434e4
Marek Jóźwiak <168720167+66HEX@users.noreply.github.com>
2025-05-18 02:05:00 +0200

    init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Scene from './components/scene'
+
+function App() {
+  return (
+    <div className='w-screen h-screen bg-black'> 
+      <Scene />
+    </div>
+  )
+}
+
+export default App
